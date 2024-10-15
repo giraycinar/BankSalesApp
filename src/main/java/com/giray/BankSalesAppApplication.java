794484186a759e843c7571a9bd6a5fe95bc420a5
giraycinar <giraycinar@outlook.com>
2024-10-15 16:49:52 +0300

    First Commit

---
+++ src/main/java/com/giray/BankSalesAppApplication.java
@@ -0,0 +1,13 @@
+package com.giray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSalesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSalesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giray/BankSalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSalesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
